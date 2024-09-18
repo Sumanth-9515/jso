6397f03c5909234f845a37fbccd32562f9e7d662
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-09-18 22:06:12 +0530

    Add files via upload

---
+++ seo.js
@@ -0,0 +1,18 @@
+var Name="sumanth reddy";
+var age=23;
+var job="softwer developer";
+console.log(Name);
+console.log(age);
+console.log(job);
+
+
+var details={
+     Name:"sumanth reddy",
+     age : 23,
+     job : "softwer developer"
+}
+console.log(details.age);
+
+
+var family = [{ father: "Srinivas", work:{ work: "farmer" }}, { mother: "Sujatha", a:{ work: "farmer" } }, { sister: "lilly" }]
+console.log(family);
